2a14f3cc0ce36fdf33beff99b48d631b008139ad
sofikul sk <rajibsk563@gmail.com>
2024-05-24 20:09:11 +0530

    this is a basic js code

---
+++ 01Basics/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+const name = "rajib sk";
+console.log(name);
---
+++ 01Basics/varLetConst.js
@@ -0,0 +1,5 @@
+const name = "rajib sk";
+const roll = 1234;
+const id = 2;
+const email = "rajibsk@gmail.com";
+console.table([name, roll, id, email]);
